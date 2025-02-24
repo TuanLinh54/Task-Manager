a338b7d11bbda83365946e0adcd321a37e5c58ee
TuanLinh54 <tlinh020405@gmail.com>
2025-02-24 13:02:41 +0700

    22/02/2025 to 24/02/2025, create project and build login page, dashboard page

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import clsx from "clsx";
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ icon, className, label, type, onClick = () => {} }) => {
+  return (
+    <button
+      type={type || "button"}
+      className={clsx("px-3 py-2 outline-none", className)}
+    >
+      <span>{label} </span>
+      {icon && icon}
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/src/pages/Tasks.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Tasks = () => {
+  return <div>Tasks</div>;
+};
+
+export default Tasks;
